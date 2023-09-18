a1a4cb8820b00d08845e6c2410d66bac8e60c4a8
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-18 17:29:43 +0100

    Title: Task10, fix pycodestyle
    Body: Task10, fix pycodestyle
    
    Co-authored-by: Fuzzworth <nyangasi.m@hotmail.com>

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
