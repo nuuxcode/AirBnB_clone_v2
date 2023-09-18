bf04495ae1db5555182124f8eea790294b577667
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-09-18 18:54:33 +0100

    Title: Fix output for task5, adding Filestorage case for all classes add getter attribute cities for state (task6)
    Body: Fix output for task5, adding Filestorage case for all classes
    add getter attribute cities for state (task6)
    
    Co-authored-by: Fuzzworth <nyangasi.m@hotmail.com>

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,16 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity',
-                                   back_populates="amenities")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place', secondary='place_amenity',
+                                       back_populates="amenities")
+    else:
+        name = ""
--- models/review.py
+++ models/review.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, Base
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    if storage_type == "db":
+        pass  # review code
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
