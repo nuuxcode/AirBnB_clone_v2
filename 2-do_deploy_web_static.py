761fbce351197dcf60390776eeecb2112867683c
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-04 23:34:19 +0100

    Task2 fix pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,12 +7,13 @@ import os
 
 env.hosts = ['18.207.1.87', '52.206.189.175']
 
+
 @task
 def do_pack():
     """ method doc
         sudo fab -f 1-pack_web_static.py do_pack
     """
-    formatted_dt = f"$(date +%Y%m%d%H%M%S)"
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
     mkdir = "mkdir -p versions"
     path = f"versions/web_static_{formatted_dt}.tgz"
     print(f"Packing web_static to versions/web_static_{formatted_dt}.tgz")
