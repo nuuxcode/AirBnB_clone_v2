3fb694ed8ef9fc06bff8755b25b817960cdde3b9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-04 20:30:13 +0100

    Task1 add doc for method

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 @task
 def do_pack():
+    """ method doc
+    """
     formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
     print(f"Packing web_static to versions/web_static_{formatted_dt}.tgz")
     local(f"mkdir -p versions && tar -cvzf versions/web_static_{formatted_dt}.tgz web_static")
