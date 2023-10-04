480f0104b20994364d6796ceb2899e586095a008
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-04 18:29:03 +0100

    Task0 fixing shellcheck ading trap

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-if ! service nginx status > /dev/null 2>&1; then
+if ! command -v nginx &> /dev/null; then
     sudo apt update
     sudo apt install nginx -y
-
+fi
 sudo mkdir -p "/data/web_static/releases/test/"
 sudo mkdir -p "/data/web_static/shared/"
 
@@ -22,6 +22,14 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
+sudo wget -q -O /etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
+config="/etc/nginx/sites-available/default"
+echo 'Hello World!' | sudo tee /var/www/html/index.html > /dev/null
+sudo sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' $config
+sudo sed -i '/^}$/i \ \n\tlocation @404 {return 404 "Ceci n'\''est pas une page\\n";}' $config
+sudo sed -i 's/=404/@404/g' $config
+sudo sed -i "/^server {/a \ \tadd_header X-Served-By $HOSTNAME;" $config
+sudo sed -i '/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}' $config
+
 sudo service nginx restart
-
-
+trap 'exit 0' ERR
