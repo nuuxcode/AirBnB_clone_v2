bde4f08b811bf0a3bc5aaa2f65e9c07b037e5531
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-05 15:40:37 +0100

    Fix small stuff

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,7 +8,7 @@ import os
 env.hosts = ['18.207.1.87', '52.206.189.175']
 
 
-@task
+@runs_once
 def do_pack():
     """ method doc
         sudo fab -f 1-pack_web_static.py do_pack
@@ -29,26 +29,28 @@ def do_deploy(archive_path):
         archive_path=versions/web_static_20231004201306.tgz
         -i ~/.ssh/id_rsa -u ubuntu
     """
-    if not os.path.exists(archive_path):
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
-    fn_with_ext = os.path.basename(archive_path)
-    fn_no_ext, ext = os.path.splitext(fn_with_ext)
-    dpath = "/data/web_static/releases/"
-    put(archive_path, "/tmp/")
-    run("rm -rf {}{}/".format(dpath, fn_no_ext))
-    run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-    run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-    run("rm /tmp/{}".format(fn_with_ext))
-    run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-    run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-    run("rm -rf /data/web_static/current")
-    run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-    print("New version deployed!")
-    return True
 
 
 @task
-@runs_once
 def deploy():
     """ method doc
         sudo fab -f 1-pack_web_static.py do_pack
@@ -56,7 +58,4 @@ def deploy():
     path = do_pack()
     if path is None:
         return False
-    for host in env.hosts:
-        env.host_string = host
-        do_deploy(path)
-    return True
+    return do_deploy(path)
