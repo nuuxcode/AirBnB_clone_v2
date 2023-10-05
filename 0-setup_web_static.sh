ca2f1c53b567be3112b7756ac4cf8a54c64a1b1f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-05 22:06:14 +0100

    Task5 testing

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-#trap 'exit 0' ERR
+trap 'exit 0' ERR
 
 if ! command -v nginx &> /dev/null; then
     sudo apt update
@@ -10,7 +10,7 @@ fi
 sudo mkdir -p "/data/web_static/releases/test/"
 sudo mkdir -p "/data/web_static/shared/"
 
-body_content="Web site under construction!"
+body_content="Holberton School Web site under construction!"
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
 html_content="<html>
   <head></head>
@@ -25,13 +25,13 @@ ln -s /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-#sudo wget -q -O /etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
+sudo wget -q -O /etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
 config="/etc/nginx/sites-available/default"
-#echo 'Hello World!' | sudo tee /var/www/html/index.html > /dev/null
-#sudo sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' $config
-#sudo sed -i '/^}$/i \ \n\tlocation @404 {return 404 "Ceci n'\''est pas une page\\n";}' $config
-#sudo sed -i 's/=404/@404/g' $config
-#sudo sed -i "/^server {/a \ \tadd_header X-Served-By $HOSTNAME;" $config
+echo 'Holberton School Hello World!' | sudo tee /var/www/html/index.html > /dev/null
+sudo sed -i '/^}$/i \ \n\tlocation \/redirect_me {return 301 https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4;}' $config
+sudo sed -i '/^}$/i \ \n\tlocation @404 {return 404 "Ceci n'\''est pas une page\\n";}' $config
+sudo sed -i 's/=404/@404/g' $config
+sudo sed -i "/^server {/a \ \tadd_header X-Served-By $HOSTNAME;" $config
 sudo sed -i '/^server {/a \ \n\tlocation \/hbnb_static {alias /data/web_static/current/;index index.html;}' $config
 
 sudo service nginx restart
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,25 +1,24 @@
 # instal and configure nginx
-if ! which('nginx') {
-  exec { 'update':
-    command     => '/usr/bin/apt-get update',
-  }
-  -> package { 'nginx':
-    ensure => installed,
-  }
+exec { 'update':
+  command     => '/usr/bin/apt-get update',
+  onlyif      => '/usr/bin/which nginx',
 }
--> exec { 'run':
+-> package { 'nginx':
+  ensure => installed,
+}
+-> exec { 'run1':
   command => '/usr/bin/mkdir -p "/data/web_static/releases/test/" "/data/web_static/shared/"',
 }
--> exec { 'run':
+-> exec { 'run2':
   command => '/usr/bin/echo "Hi!" | sudo tee /data/web_static/releases/test/index.html > /dev/null',
 }
--> exec { 'run':
+-> exec { 'run3':
   command => '/usr/bin/rm -rf /data/web_static/current',
 }
--> exec { 'run':
+-> exec { 'run4':
   command => '/usr/bin/ln -s /data/web_static/releases/test/ /data/web_static/current',
 }
--> exec { 'run':
+-> exec { 'run5':
   command => '/usr/bin/chown -R ubuntu:ubuntu /data/',
 }
 -> file_line { 'location_hbnb_static':
@@ -28,6 +27,6 @@ if ! which('nginx') {
   line     => 'location /hbnb_static {alias /data/web_static/current/;index index.html;}',
   multiple => false,
 }
--> exec { 'run':
+-> exec { 'run6':
   command => '/usr/sbin/service nginx restart',
 }
