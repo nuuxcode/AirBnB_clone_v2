92e3e24c504fabcfbdb4f8a3d7663960b2aa82a9
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-05 01:16:28 +0100

    Task2 finish

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,27 +27,22 @@ def do_deploy(archive_path):
         archive_path=versions/web_static_20231004201306.tgz
         -i ~/.ssh/id_rsa -u ubuntu
     """
-    if os.path.exists(archive_path):
+    try:
+        if os.path.exists(archive_path) is False:
+            return False
         fn_with_ext = os.path.basename(archive_path)
         fn_no_ext, ext = os.path.splitext(fn_with_ext)
         dpath = "/data/web_static/releases/"
-        put_result = put(archive_path, "/tmp/")
-        rm_result = run(f"rm -rf {dpath}{fn_no_ext}/")
-        mkdir_result = run(f"mkdir -p {dpath}{fn_no_ext}/")
-        tar_result = run(f"tar -xzf /tmp/{fn_with_ext} -C {dpath}{fn_no_ext}/")
-        rm_tmp_result = run(f"rm /tmp/{fn_with_ext}")
-        mv_r = run(f"mv {dpath}{fn_no_ext}/web_static/* {dpath}{fn_no_ext}/")
-        rm_web_static_result = run(f"rm -rf {dpath}{fn_no_ext}/web_static")
-        rm_current_result = run(f"rm -rf /data/web_static/current")
-        ln_result = run(f"ln -s {dpath}{fn_no_ext}/ /data/web_static/current")
-        if (
-            put_result.failed or rm_result.failed or mkdir_result.failed or
-            tar_result.failed or rm_tmp_result.failed or mv_r.failed or
-            rm_web_static_result.failed or rm_current_result.failed or
-            ln_result.failed
-        ):
-            return False
-        else:
-            return True
-    else:
+        put(archive_path, "/tmp/")
+        run(f"rm -rf {dpath}{fn_no_ext}/")
+        run(f"mkdir -p {dpath}{fn_no_ext}/")
+        run(f"tar -xzf /tmp/{fn_with_ext} -C {dpath}{fn_no_ext}/")
+        run(f"rm /tmp/{fn_with_ext}")
+        run(f"mv {dpath}{fn_no_ext}/web_static/* {dpath}{fn_no_ext}/")
+        run(f"rm -rf {dpath}{fn_no_ext}/web_static")
+        run(f"rm -rf /data/web_static/current")
+        run(f"ln -s {dpath}{fn_no_ext}/ /data/web_static/current")
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
