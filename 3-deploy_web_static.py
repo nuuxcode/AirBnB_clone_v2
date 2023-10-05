9eb06c1302bf6ea602b30643c7ae2bfd67d91e5b
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-05 14:38:24 +0100

    Task3 Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ module doc
 """
-from fabric.api import task, local, env, put, run
+from fabric.api import task, local, env, put, run, runs_once
 from datetime import datetime
 import os
 
@@ -29,32 +29,33 @@ def do_deploy(archive_path):
         archive_path=versions/web_static_20231004201306.tgz
         -i ~/.ssh/id_rsa -u ubuntu
     """
-    try:
-        if not os.path.exists(archive_path):
-            return False
-        fn_with_ext = os.path.basename(archive_path)
-        fn_no_ext, ext = os.path.splitext(fn_with_ext)
-        dpath = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("rm -rf {}{}/".format(dpath, fn_no_ext))
-        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
-        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
-        run("rm /tmp/{}".format(fn_with_ext))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
-        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
-        print("New version deployed!")
-        return True
-    except Exception:
+    if not os.path.exists(archive_path):
         return False
+    fn_with_ext = os.path.basename(archive_path)
+    fn_no_ext, ext = os.path.splitext(fn_with_ext)
+    dpath = "/data/web_static/releases/"
+    put(archive_path, "/tmp/")
+    run("rm -rf {}{}/".format(dpath, fn_no_ext))
+    run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+    run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+    run("rm /tmp/{}".format(fn_with_ext))
+    run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+    run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+    run("rm -rf /data/web_static/current")
+    run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+    print("New version deployed!")
+    return True
 
 
 @task
+@runs_once
 def deploy():
-    """ method doc
-    """
     path = do_pack()
-    if (path) is None:
+    if path is None:
         return False
-    return do_deploy(path)
+
+    for host in env.hosts:
+        env.host_string = host
+        do_deploy(path)
+
+    return True
