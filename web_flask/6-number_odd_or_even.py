c35c4e5e9a3cd2174a1c4f1fac4dcc226a54219e
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-20 11:50:43 +0100

    Task6 Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,9 +37,14 @@ def number(n):
     return '{} is a number'.format(n)
 
 
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    return render_template('5-number.html', number=n)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """ def doc """
+    if n % 2 == 0:
+        parity = 'even'
+    else:
+        parity = 'odd'
+    return render_template('6-number_odd_or_even.html', number=n, parity=parity)
 
 
 if __name__ == "__main__":
