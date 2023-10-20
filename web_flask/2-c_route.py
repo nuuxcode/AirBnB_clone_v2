26445b21599f51b50ddfab16555d327db2b7550f
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-10-20 09:48:21 +0100

    Task2 fix typo

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,7 +20,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """ def doc """
-    return 'c {}'.format(text.replace("_", " "))
+    return 'C {}'.format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
